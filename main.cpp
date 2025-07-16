ed28941106af56621c263167c5a022afbd36f45b
Victor <112712496+kip999W@users.noreply.github.com>
2025-07-16 21:05:41 +0700

    Реализация класса Database для управления информацией о пользователях
    - Класс Database управляет данными пользователей, включая добавление, чтение, изменение, удаление и сохранение информации о пользователях.
    - Определена структура UserInfo для хранения атрибутов пользователей, таких как имя пользователя, фамилия, отчество, группа, номер телефона и дата рождения (день, месяц, год).
    - Реализованы методы для:
      - Валидации номеров телефонов и дат рождения.
      - Добавления новой информации о пользователе с валидацией данных.
      - Чтения зашифрованных данных пользователей из файла и их расшифровки.
      - Изменения существующих данных о пользователе.
      - Удаления данных пользователя на основе ввода пользователя.
      - Сохранения данных о пользователе в файл с шифрованием.
      - Вывода деталей пользователя на консоль.
    
    - Добавлена обработка ошибок для операций с файлами и недопустимых входных данных.
    - Обеспечено управление памятью с помощью динамического выделения и освобождения данных о пользователях.
    - Интегрирован консольный интерфейс для взаимодействия с пользователем.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 #include "NavigatorMenu.h"
 #include "Encryptor.h"
 
 bool checkFileExists(const std::string& filename);
+
+class Database {
+private:
+    struct UserInfo {
+        std::string username;
+        std::string surname;
+        std::string patronymic;
+        std::string group;
+        std::string phoneNumber;
+        int day, month, year;
+    };
+
+    const std::string f_DATABASE = "./files/database/";
+    UserInfo* users;
+    int numberOfData = 0;
+
+    bool isValidPhoneNumber(const std::string& phoneNumber);
+    bool isValidDate(int day, int month, int year);
+
+    void addUser(UserInfo* (&users), int& n);
+    void dataReading(UserInfo* (&users), int& n, const std::string& filename);
+    void dataChange(UserInfo* (&users), int& n);
+    void saveData(UserInfo* users, int n, const std::string& filename);
+    void copyData(UserInfo* (&data_new), UserInfo* (&data_old), int n);
+    void deleteData(UserInfo* (&data), int& n);
+    void printUser(UserInfo* (&users), int& n) const;
+
+public:
+    Database() {
+        users = new UserInfo[numberOfData];
+    }
+
+    void displayDatabaseMenu();
+
+    ~Database() {
+        delete[] users;
+    }
+
+};
 
 class Authorization {
 private:
@@ -40,6 +80,306 @@ int main() {
 bool checkFileExists(const std::string& filename) {
     std::ifstream inFile(filename);
     return inFile.is_open();
+}
+
+bool Database::isValidPhoneNumber(const std::string& phoneNumber) {
+    std::regex phoneRegex(R"(^(\+?\d{1,3}[- ]?)?\d{10}$)");
+    return std::regex_match(phoneNumber, phoneRegex);
+}
+
+bool Database::isValidDate(int day, int month, int year) {
+    if (year < 1900 || year > std::time(nullptr)) {
+        std::cout << "Год вне допустимого диапазона!" << std::endl;
+        system("pause");
+        system("cls");
+        return false;
+    }
+
+    if (month < 1 || month > 12) {
+        std::cout << "Неверный месяц!" << std::endl;
+        system("pause");
+        system("cls");
+        return false;
+    }
+
+    int daysInMonth[] = {31, (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    return day > 0 && day <= daysInMonth[month - 1];
+}
+
+void Database::addUser(UserInfo* (&users), int& n) {
+    do {
+        std::cout << "Введите кол-во данных (от 1 до 30): "; std::cin >> n;
+        system("cls");
+
+        if (n < 1 || n > 30) {
+            std::cout << "Попробуйте еще раз!" << std::endl;
+            system("pause");
+            system("cls");
+        }
+
+    } while (n < 1 || n > 30);
+
+    users = new UserInfo[n];
+
+    for (int i = 0; i < n; i++) {
+        bool isValid;
+
+        do {
+            isValid = true;
+
+            std::cout << "Введите имя: "; std::cin >> users[i].username;
+            std::cout << "Введите фамилию: "; std::cin >> users[i].surname;
+            std::cout << "Введите отчество: "; std::cin >> users[i].patronymic;
+            std::cout << "Введите группу: "; std::cin >> users[i].group;
+            std::cout << "Введите номер телефона: "; std::cin >> users[i].phoneNumber;
+
+            if (!isValidPhoneNumber(users[i].phoneNumber)) {
+                std::cout << "Неверный номер телефона. Попробуйте еще раз." << std::endl << std::endl;
+                isValid = false;
+                continue;
+            }
+
+            std::cout << "Введите дату рождения (ДД): "; std::cin >> users[i].day;
+            std::cout << "Введите месяц рождения (ММ): "; std::cin >> users[i].month;
+            std::cout << "Введите год рождения (ГГГГ): "; std::cin >> users[i].year;
+
+            if (!isValidDate(users[i].day, users[i].month, users[i].year))
+                isValid = false;
+
+        } while (!isValid);
+
+        system("cls");
+    }
+
+    std::cout << "Данные успешно добавлены!" << std::endl;
+}
+
+void Database::dataReading(UserInfo* (&users), int& n, const std::string& filename) {
+    Encryptor encryptor("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678906+-_!@#$%^&*(", 7);
+    std::ifstream inFile(filename);
+
+    if (inFile.is_open()) {
+        std::string encryptedN;
+        inFile >> encryptedN;
+        n = std::stoi(encryptor.decrypt(encryptedN));
+
+        users = new UserInfo[n];
+
+        for (int i = 0; i < n; i++) {
+            std::string encryptedSurname, encryptedUsername, encryptedPatronymic;
+            std::string encryptedGroup;
+            std::string encryptedPhoneNumber;
+            std::string encryptedDay, encryptedMonth, encryptedYear;
+
+            inFile >> encryptedSurname;
+            inFile >> encryptedUsername;
+            inFile >> encryptedPatronymic;
+            inFile >> encryptedGroup;
+            inFile >> encryptedPhoneNumber;
+            inFile >> encryptedDay;
+            inFile >> encryptedMonth;
+            inFile >> encryptedYear;
+
+            users[i].surname = encryptor.decrypt(encryptedSurname);
+            users[i].username = encryptor.decrypt(encryptedUsername);
+            users[i].patronymic = encryptor.decrypt(encryptedPatronymic);
+            users[i].group = encryptor.decrypt(encryptedGroup);
+            users[i].phoneNumber = encryptor.decrypt(encryptedPhoneNumber);
+            users[i].day = std::stoi(encryptor.decrypt(encryptedDay));
+            users[i].month = std::stoi(encryptor.decrypt(encryptedMonth));
+            users[i].year = std::stoi(encryptor.decrypt(encryptedYear));
+        }
+
+        std::cout << "Данные успешно прочитаны!" << std::endl;
+    } else
+        std::cerr << "Ошибка при попытке открытия файла!" << std::endl;
+
+    inFile.close();
+}
+
+void Database::dataChange(UserInfo* (&users), int& n) {
+    int _n;
+    std::cout << "Введите номер элемента, который вы хотите изменить (от 1 до " << n << "): ";
+    std::cin >> _n;
+    _n--;
+
+    system("cls");
+
+    if (_n >= 0 && _n < n) {
+        bool isValid;
+
+        do {
+            isValid = true;
+
+            std::cout << "Введите имя: "; std::cin >> users[_n].username;
+            std::cout << "Введите фамилию: "; std::cin >> users[_n].surname;
+            std::cout << "Введите отчество: "; std::cin >> users[_n].patronymic;
+            std::cout << "Введите группу: "; std::cin >> users[_n].group;
+            std::cout << "Введите номер телефона: "; std::cin >> users[_n].phoneNumber;
+
+            if (!isValidPhoneNumber(users[_n].phoneNumber)) {
+                std::cout << "Неверный номер телефона. Попробуйте еще раз." << std::endl << std::endl;
+                isValid = false;
+                continue;
+            }
+
+            std::cout << "Введите дату рождения (ДД): "; std::cin >> users[_n].day;
+            std::cout << "Введите месяц рождения (ММ): "; std::cin >> users[_n].month;
+            std::cout << "Введите год рождения (ГГГГ): "; std::cin >> users[_n].year;
+
+            if (!isValidDate(users[_n].day, users[_n].month, users[_n].year))
+                isValid = false;
+
+        } while (!isValid);
+
+        system("cls");
+
+        std::cout << "Данные изменены!" << std::endl;
+    } else
+        std::cout << "Введенный номер неверный!" << std::endl;
+}
+
+void Database::copyData(UserInfo* (&data_new), UserInfo* (&data_old), int n) {
+    for (int i = 0; i < n; i++)
+        data_new[i] = data_old[i];
+}
+
+void Database::deleteData(UserInfo* (&data), int& n) {
+    int _n;
+    std::cout << "Введите номер элемента, который вы хотите удалить (от 1 до " << n << "): ";
+    std::cin >> _n;
+    _n--;
+
+    system("cls");
+
+    if (_n >= 0 && _n < n) {
+        UserInfo* buf = new UserInfo[n];
+
+        copyData(buf, data, n);
+
+        --n;
+        data = new UserInfo[n];
+
+        int q = 0;
+        for (int i = 0; i <= n; ++i) {
+            if (i != _n) {
+                data[q] = buf[i];
+                ++q;
+            }
+        }
+
+        delete[] buf;
+
+        std::cout << "Данные удалены!" << std::endl;
+    } else
+        std::cout << "Ошибка!" << std::endl;
+}
+
+void Database::saveData(UserInfo* users, int n, const std::string& filename) {
+    Encryptor encryptor("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678906+-_!@#$%^&*(", 7);
+    std::ofstream outFile(filename);
+
+    if (outFile.is_open()) {
+        outFile << encryptor.encrypt(std::to_string(n)) << std::endl;
+
+        for (int i = 0; i < n; i++) {
+            outFile << encryptor.encrypt(users[i].surname) << std::endl
+                    << encryptor.encrypt(users[i].username) << std::endl
+                    << encryptor.encrypt(users[i].patronymic) << std::endl
+                    << encryptor.encrypt(users[i].group) << std::endl
+                    << encryptor.encrypt(users[i].phoneNumber) << std::endl
+                    << encryptor.encrypt(std::to_string(users[i].day)) << std::endl
+                    << encryptor.encrypt(std::to_string(users[i].month)) << std::endl
+                    << encryptor.encrypt(std::to_string(users[i].year)) << std::endl;
+        }
+
+        std::cout << "Данные сохранены!" << std::endl;
+    } else
+        std::cerr << "Ошибка при попытке открытия файла!" << std::endl;
+
+    outFile.close();
+}
+
+void Database::printUser(UserInfo* (&users), int& n) const {
+    std::cout << "Кол-во данных: " << n << std::endl << std::endl;
+
+    for (int i = 0; i < n; i++) {
+        std::cout << "Информация #" << i + 1 << std::endl;
+        std::cout << "Фамилия: " << users[i].surname << std::endl;
+        std::cout << "Имя: " << users[i].username << std::endl;
+        std::cout << "Отчество: " << users[i].patronymic << std::endl;
+        std::cout << "Группа: " << users[i].group << std::endl;
+        std::cout << "Номер телефона: " << users[i].phoneNumber << std::endl;
+        std::cout << "ДД.ММ.ГГ рождения: " << users[i].day << "." << users[i].month << "." << users[i].year << std::endl;
+        std::cout << "_______________________________________________" << std::endl;
+    }
+
+    system("pause");
+    system("cls");
+}
+
+void Database::displayDatabaseMenu() {
+    SetConsoleTitle("Database");
+
+    std::vector<std::string> items = {
+        "Добавить данные",
+        "Читать данные",
+        "Изменить данные",
+        "Удалить данные",
+        "Сохранить данные",
+        "Вывести данные",
+        "Выйти"
+    };
+
+    NavigatorMenu navigatorMenu(items);
+    navigatorMenu.navigate();
+    int currentSelection = navigatorMenu.getCurrentSelection();
+
+    std::string filename;
+
+    switch (currentSelection) {
+        case 0:
+            addUser(users, numberOfData);
+            break;
+        case 1:
+            std::cout << "Введите название файла: ";
+            std::cin >> filename;
+
+            system("cls");
+
+            dataReading(users, numberOfData, f_DATABASE + filename);
+            break;
+        case 2:
+            if (numberOfData != 0) {
+                dataChange(users, numberOfData);
+            } else
+                std::cout << "Данные пусты!" << std::endl;
+            break;
+        case 3:
+            if (numberOfData != 0) {
+                deleteData(users, numberOfData);
+            } else
+                std::cout << "Данные пусты!" << std::endl;
+            break;
+        case 4:
+            if (numberOfData != 0) {
+                std::cout << "Придумайте название для файла: ";
+                std::cin >> filename;
+
+                system("cls");
+
+                saveData(users, numberOfData, f_DATABASE + filename);
+            } else
+                std::cout << "Данные пусты!" << std::endl;
+            break;
+        case 5:
+            printUser(users, numberOfData);
+            break;
+        case 6:
+            navigatorMenu.close();
+            break;
+    }
 }
 
 std::string Authorization::readEncryptedFile(const std::string& filename, int lineNumber) {
@@ -198,7 +538,9 @@ void Authorization::displayAuthorization() {
     switch (currentSelection) {
         case 0:
             if (entrance()) {
-
+                Database database;
+                while (true)
+                    database.displayDatabaseMenu();
             }
 
             break;
